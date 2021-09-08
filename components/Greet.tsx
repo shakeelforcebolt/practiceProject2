d8eaabf0ed1fb8796bfc7b9f546e1d76a32c2784
shakeelforcebolt <89139866+shakeelforcebolt@users.noreply.github.com>
2021-09-09 00:34:42 +0530

    8-Sept-21

--- components/Greet.tsx
+++ components/Greet.tsx
@@ -8,6 +8,7 @@ const greet = (props : GreetProps) => {
     return (
         <div>
             <h1>Greet Heading!!!!!</h1>
+            <h1>Greet Heading!!!!!</h1>
             <h1>In Props Name Call: {props.name}</h1>
 
         </div>
